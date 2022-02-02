4d3368d3b3f29cb48680536828076776b98b6a04
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-03 03:30:56 +0900

    :bookmark: ch05 p132 | Nweet 컴포넌트로 분리

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { dbService } from "fbase";
 import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
@@ -40,9 +41,7 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         { nweets.map((nweet) => (
-          <div key={ nweet.id }>
-            <h4>{ nweet.text }</h4>
-          </div>
+          <Nweet key={ nweet.id } nweetObj={ nweet } />
         ))}
       </div>
     </>
