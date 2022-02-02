417a6944d60c3346772f2afebff01fb99ddd3204
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-02 23:38:07 +0900

    :bookmark: ch04 p104 | 네비게이션 추가, 로그아웃 추가
    - 로그아웃은 Redirect 사용하는 방법

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,23 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigate, HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      { isLoggedIn && <Navigation /> }
       <Routes>
         { isLoggedIn ? (
-          <Route exact path="/" element={<Home />} />
+          <>
+            <Route exact path="/" element={ <Home /> } />
+            <Route exact path="/profile" element={ <Profile /> }/>
+          </>
         ) : (
-          <Route exact path="/" element={<Auth />} />
+          <Route exact path="/" element={ <Auth /> } />
         )}
+        <Route path="*" element={ <Navigate replace to="/" /> } />
       </Routes>
     </Router>
   )
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,14 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+import { signOut } from "firebase/auth";
+
+const Profile = () => {
+  const onLogOutClick = () => signOut(authService);
+
+  return (
+    <>
+      <button onClick={ onLogOutClick }>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
