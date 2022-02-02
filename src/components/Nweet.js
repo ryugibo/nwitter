8bfa0fb8b0c0a8ebbaeb5472f56253a21639752a
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-03 03:40:51 +0900

    :bookmark: ch05 p140 | 삭제 기능

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,7 +1,22 @@
-const Nweet = ({ nweetObj }) => {
+import { dbService } from "fbase";
+import { doc, deleteDoc } from "firebase/firestore";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    if (ok) {
+      await deleteDoc(doc(dbService, "nweets", nweetObj.id));
+    }
+  };
   return (
     <div>
       <h4>{ nweetObj.text }</h4>
+      { isOwner && (
+        <>
+          <button onClick={ onDeleteClick }>Delete Nweet</button>
+          <button>Edit Nweet</button>
+        </>
+      )}
     </div>
   );
 };
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -41,7 +41,7 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         { nweets.map((nweet) => (
-          <Nweet key={ nweet.id } nweetObj={ nweet } />
+          <Nweet key={ nweet.id } nweetObj={ nweet } isOwner={ nweet.creatorId === userObj.uid } />
         ))}
       </div>
     </>
