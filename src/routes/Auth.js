742497a426d7eb6032c23269759d07c8bcf9db58
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-02 23:27:25 +0900

    :bookmark: ch04 p98 | 소셜 로그인 연동

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { authService } from "fbase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -32,6 +38,17 @@ const Auth = () => {
   }
   const toggleAccount = () => setNewAccount((prev) => !prev);
 
+  const onSocialClick = async (event) => {
+    const { target: { name } } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+  };
+
   return (
     <div>
       <form onSubmit={ onSubmit }>
@@ -44,8 +61,8 @@ const Auth = () => {
         { newAccount ? "Sign In" : "Create Account" }
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={ onSocialClick } name="google">Continue with Google</button>
+        <button onClick={ onSocialClick } name="github">Continue with Github</button>
       </div>
     </div>
   )
