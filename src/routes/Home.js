676e1cb97ec9f46b41a8062559d7a56bfc84b37d
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-03 03:08:05 +0900

    :bookmark: ch05 p116 | 파이어스토어 연동, 데이터 생성 로직

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,31 @@
-const Home = () => <span>Home</span>;
+import { useState } from "react";
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
+
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const { target: { value } } = event;
+    setNweet(value);
+  };
+  
+  return (
+    <form onSubmit={ onSubmit }>
+      <input value={ nweet } onChange={ onChange } type="text" placeholder="What's on your mind?" maxLength={ 120 } />
+      <input type="submit" value="Nweet" />
+    </form>
+  );
+};
 
 export default Home;
