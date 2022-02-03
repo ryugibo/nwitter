7e37bc34164fecfb9046e182cec3fccc98e5731c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-03 16:44:09 +0900

    :bookmark: ch07 p181 | 프로필 페이지에서 내가 작성한 글만 가져오기
    - 프로필 내 글 목록 표시부분은 스스로

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,26 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
 import { signOut } from "firebase/auth";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const Navigate = useNavigate();
   const onLogOutClick = () => {
     signOut(authService);
     Navigate("/");
   };
+
+  const getMyNweets = async () => {
+    const nweets = await getDocs(query(collection(dbService, "nweets"), where("creatorId", "==", userObj.uid), orderBy("createdAt", "asc")));
+
+    console.log(nweets.docs.map((doc) => doc.data()));
+    // @TODO: 내 글 목록 표시하기
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
