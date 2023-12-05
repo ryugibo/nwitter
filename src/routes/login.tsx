ea8960c71e5331e29385caaedb15dce6a6ba8545
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-12-05 15:14:37 +0900

    Github login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -11,6 +11,7 @@ import {
   Title,
   Wrapper,
 } from "../components/auth-components";
+import GithubButton from "../components/github-button";
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -73,6 +74,7 @@ export default function CreateAccount() {
         Don't have an account?{" "}
         <Link to="/create-account">Create one &rarr;</Link>
       </Switcher>
+      <GithubButton />
     </Wrapper>
   );
 }
