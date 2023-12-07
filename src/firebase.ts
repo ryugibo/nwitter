2c0f0b982c57ad27f1bf6a506aa9e4c77cc23dc3
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-12-07 23:50:15 +0900

    change firebase app

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsrBen6pmBDh50o0OE7AOU5MTc7tmBC4k",
+  apiKey: "AIzaSyAJ4d4CPCJq8j74wxz9080hkHfOjNLLCys",
   authDomain: "nwitter-5c274.firebaseapp.com",
   projectId: "nwitter-5c274",
   storageBucket: "nwitter-5c274.appspot.com",
   messagingSenderId: "382513733157",
-  appId: "1:382513733157:web:4ff6e221f1158c01eb69cc",
+  appId: "1:382513733157:web:b4b5a775668fafdbeb69cc",
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
