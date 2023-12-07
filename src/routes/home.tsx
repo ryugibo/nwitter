ba244667dc78eb7e48b8e4e52e2832e11ca89bb6
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-12-07 23:10:21 +0900

    Navigation Bar

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,3 @@
-import { auth } from "../firebase";
-
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-  };
-  return (
-    <h1>
-      Home! <button onClick={logOut}>Logout</button>
-    </h1>
-  );
+  return <h1>Home!</h1>;
 }
